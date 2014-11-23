ac7926934c18084bf48828578561f818035995b1
hasherr <eklein217@gmail.com>
2014-11-23 01:12:46 -0500

    Created packages and test main

---
+++ src/com/hasherr/stegory/core/Starter.java
@@ -0,0 +1,12 @@
+package com.hasherr.stegory.core;
+
+/**
+ * Created by Evan on 11/23/2014.
+ */
+public class Starter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Stegory");
+    }
+}
